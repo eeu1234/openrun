4388fc5b337829c8d840ef055c183510c339b782
eeu1234 <eeu4327@gmail.com>
2022-10-28 05:36:01 +0900

    VUE 연결완료

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Models\User;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\DB;
 
-class Controller extends BaseController
+class Controller extends \Illuminate\Routing\Controller
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function index()
+    {
+        $test = User::se;
+
+        \Log::info($test);
+
+        return view('welcome');
+    }
 }
