5c46a65e1ad2ec1a8d592b316171ed002abf9422
Hong-yeonggwang <chamsk0808@naver.com>
2022-11-23 06:58:04 +0900

    구조변경: resources/js/components/user/search/search.js삭제후 app.js에서 뷰컴포터는 통합관리

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,19 @@
 import { createApp } from 'vue'
-import Welcome from './components/Welcome.vue';
 import productView from './components/user/product/productView.vue';
-import test from './components/test.vue';
 import './bootstrap';
 import '../css/app.css'
 
-//createApp(Welcome).mount("#app");
-const app = createApp({});
-//createApp(productView).mount("#productView");
-app.component('productView',productView);
-// $(document).ready(function(){
-//     $('#menu_button').click(function(){
-//         $("#test").css("display","block");
-//         console.log("hi");
-//     });
-// });
+//검색 관련 뷰파일
+import header_search from "./components/user/search/header_search.vue";
+import sideMenu from "./components/user/search/sideMenu.vue";
+import search_main from "./components/user/search/search_main.vue";
+
+// const app = createApp({});
+//
+// app.component('productView',productView);
+
+createApp(header_search).mount("#header");
+createApp(search_main).mount("#searchMain");
+createApp(sideMenu).mount("#sideMenu");
+
+
