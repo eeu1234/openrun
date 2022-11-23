3c2818e5e6ec91c646ac391ada66a8b33b183d1e
Hong-yeonggwang <chamsk0808@naver.com>
2022-11-23 13:48:48 +0900

    css작업: sidebar css작업.

--- resources/js/components/user/search/header_search.vue
+++ resources/js/components/user/search/header_search.vue
@@ -34,7 +34,7 @@ $(document).ready(function(){
 });
 function slideSideMenu(){
     $("#sideBar").css("display","block");
-    $("#sideMenu").css("width","25%");
+    $("#sideMenu").css("width","20%");
     $("#sideMenu").css("height","100%");
     console.log("hi");
 
--- resources/js/components/user/search/sideMenu.vue
+++ resources/js/components/user/search/sideMenu.vue
@@ -1,6 +1,16 @@
 <template>
-    <div id = "sideBar" class =  "w-full h-full bg-gray-500 absolute hidden">
-        <div id = "closeButton" class = "h-20 w-2/3 bg-yellow-50 m-auto my-5"></div>
+    <div id = "sideBar" class =  "w-full h-full absolute hidden" style = "background-color:rgba(127,127,127,0.9);">
+        <div id = "topArae" class = "w-full h-40">
+            <div id = "closeButton" class = "mt-5 mx-7  float-left">
+                <img class = "h-12" src="/img/exit.png">
+            </div>
+<!--            <div id = "sideBarTitle" class = "bg-green-50 float-left">-->
+<!--                <img class = "h-32" src="/img/chanel_logo.png">-->
+<!--            </div>-->
+            <div class = "clear-both"></div>
+        </div>
+        <div id = "menuIndex" class = "m-auto w-8/12 my-5 text-xl">메인페이지</div>
+        <div id = "menuIndex" class = "m-auto w-8/12 my-5 text-xl">검색</div>
     </div>
 </template>
 
