2a84d1b2d2568c6a41fc8651ff29e635b2295013
eeu1234 <eeu4327@gmail.com>
2022-12-27 16:07:17 +0900

    2022-12-27 최종수정

--- resources/js/components/user/search/header_search.vue
+++ resources/js/components/user/search/header_search.vue
@@ -1,13 +1,18 @@
 <template>
     <div class = "bg-black text-white text-center text-xl" >
         <div class="w-full pt-2 h-28 px-14 relative">
-            <div class = "float-left w-72 ">
-                <img class = "h-14 m-auto" src="/img/chanel_logo.png">
+            <div class = "float-left w-full" >
+                <img class = "h-14 m-auto" src="/img/chanel_logo.png" @click="main">
                 <div class="font-sans font-extrabold pb-3">
                     NO WHERE
                 </div>
-                <button id = "menu_button" class = "absolute top-0 right-0 w-2/12 pt-8 mr-6" @click="toggleSideMenu = !toggleSideMenu">
+<!--                <button id = "menu_button" class = "absolute top-0 right-0 w-2/12 pt-8 mr-2" @click="toggleSideMenu = !toggleSideMenu">
                     <img class = "h-8" src="/img/menu.png">
+                </button>-->
+                <button id = "menu_button" class = "absolute top-0 right-0 w-2/12 pl-4 pt-8" @click="goSearch">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-8 h-8">
+                        <path fill-rule="evenodd" d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z" clip-rule="evenodd" />
+                    </svg>
                 </button>
             </div>
 
@@ -16,7 +21,7 @@
 
     </div>
 
-    <div id = "sideBar" class =  "w-52 h-full absolute z-50 bg-gray-200 top-0 right-0" v-if="toggleSideMenu">
+    <div id = "sideBar" class =  "w-40 h-full absolute z-50 bg-gray-200 top-0 right-0" v-if="toggleSideMenu">
         <div id = "topArae" class = "w-full text-lg mt-4 ml-4">
             <button id = "closeButton" class = "mt-5 mb-5 " @click="toggleSideMenu = !toggleSideMenu">
                 <img class = "h-8" src="/img/exit.png">
@@ -43,7 +48,12 @@ export default {
     created() {
     },
     methods: {
-
+        main : function(){
+            location.href='/';
+        },
+        goSearch : function(){
+            location.href='/searchMain';
+        },
     }
 }
 </script>
