b4c521b69cd1ca7aadac87859deb49402c79b99b
doyounglee <eeu1234@naver.com>
2023-02-11 04:55:34 +0900

    favicon / title 설정

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,6 +5,9 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Chanel::OPENRUN</title>
+    <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/img/favicon.ico" type="image/x-icon">
 </head>
 <body class="antialiased max-w-5xl w-full m-auto " style = "min-width:410px;">
 <div id="app" class="w-full max-w-md m-auto">
